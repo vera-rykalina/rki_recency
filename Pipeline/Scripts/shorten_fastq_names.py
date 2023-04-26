8e91fe2e6eba915ba56bab26cf8250d7cd45bbd3
vera-rykalina <verarykalina@gmail.com>
2023-04-26 18:05:01 +0200

    Remove file

--- Pipeline/Scripts/shorten_fastq_names.py
+++ Pipeline/Scripts/shorten_fastq_names.py
@@ -7,7 +7,7 @@ infilename = sys.argv[1]
 outfilename = sys.argv[2]
 
 
-pattern = r"\d{6}_\d{2}-\d{5}_(HIV\d{2}-\d{5})_\d{2}_\d{3}_\w{3}_\L[0-9]{3}_(R\d)_\d{3}.(fastq.gz)"
+pattern = r"\d{6}_\d{2}-\d{5}_(HIV\d{2}-\d{5})_\d{2}_\d{3}_\w{3}_L[0-9]{3}_(R\d)_\d{3}.(fastq.gz)"
 substring = r"\1_\2.\3"
 
 #names = ""
