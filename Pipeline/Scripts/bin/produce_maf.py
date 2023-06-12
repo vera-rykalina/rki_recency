5d042a3e67b11883fd94459b32fd540908c19983
vera-rykalina <verarykalina@gmail.com>
2023-06-12 17:42:12 +0200

    Modify produce_maf.py

--- Pipeline/Scripts/bin/produce_maf.py
+++ Pipeline/Scripts/bin/produce_maf.py
@@ -46,15 +46,15 @@ df = df[df["Position in B.FR.83.HXB2_LAI_IIIB_BRU.K03455"] != "-"]
 df.rename({"Position in B.FR.83.HXB2_LAI_IIIB_BRU.K03455": "pos"}, axis=1, inplace=True)
 
 # Rename MAF column -> ID (sample ID)
-df.rename({"MAF": name2}, axis=1, inplace=True)
+df.rename({"MAF": name2 + "_MAF"}, axis=1, inplace=True)
 
 
-df.reset_index(drop=True, inplace=True)
-df = df.T
+#df.reset_index(drop=True, inplace=True)
+#df = df.T
 
 print(df)
 #print(df.info())
-print(df.shape[0])
-print(df[df.columns[0]].count())
+#print(df.shape[0])
+#print(df[df.columns[0]].count())
 # Prepare a .csv file
-df.to_csv(name2 + "_MAF" + ".csv", sep=",", index=True, encoding="utf-8")
+df.to_csv(name2 + "_MAF" + ".csv", sep=",", index=False, encoding="utf-8")
