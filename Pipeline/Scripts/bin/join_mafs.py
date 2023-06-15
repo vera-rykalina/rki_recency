daeb4c2ca02a5132e8df582f97259da3108f67ea
vera-rykalina <verarykalina@gmail.com>
2023-06-15 11:41:27 +0200

    Update list of dfs

--- Pipeline/Scripts/bin/join_mafs.py
+++ Pipeline/Scripts/bin/join_mafs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/Caskroom/miniconda/base/bin/python
 
 # Import libraries
 import pandas as pd
@@ -6,23 +6,16 @@ import sys
 import functools as ft
 
 
-infilename = sys.argv[1]
+#infilename = sys.argv[1]
 #outfilename = sys.argv[2]
 
-# Import libraries
-import pandas as pd
-import sys
-
-
-infilename = sys.argv[1]
-outfilename = sys.argv[2]
 
 dfs = []
 for infilename in sys.argv[1:]:
     dfs.append(pd.read_csv(infilename, sep = ","))
 
-ref = pd.read_csv("HXB2_refdata.csv", sep = ",", index_col=False)
-ref = pd.DataFrame(ref["pos"], columns=["pos"])
+ref = pd.read_csv("HXB2_refdata.csv", sep = ",")
+#ref = pd.DataFrame(ref["pos"], columns=["pos"])
 
 #dfs = [df.set_index('pos') for df in dfs] 
 #df = dfs[0].join(dfs[1:])
@@ -30,12 +23,12 @@ ref = pd.DataFrame(ref["pos"], columns=["pos"])
 #df= ft.reduce(lambda left, right: pd.merge(left, right, on='pos'), dfs)
 #df= ft.reduce(lambda left, right: pd.merge(left, right, on = "pos", how = "outer"), dfs)
 #df = dfs[0]
-dfs[0]=ref
+dfs.insert(0, ref)
 
 df=dfs[0]
 print(len(dfs))
 for df_ in dfs[1:]:
-    df = df.merge(df_, on="pos")
+    df = df.merge(df_, on="pos", how="left")
 
 
 df.to_csv("joined" + "_MAF" + ".csv", sep=",", index=False, encoding="utf-8")
