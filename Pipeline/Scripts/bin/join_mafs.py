845e394854dbb7f9727d28a208dc2ff3fe8a2e9c
vera-rykalina <verarykalina@gmail.com>
2023-06-16 19:46:01 +0200

    Transpose maf tables

--- Pipeline/Scripts/bin/join_mafs.py
+++ Pipeline/Scripts/bin/join_mafs.py
@@ -1,40 +1,40 @@
-#!/usr/local/Caskroom/miniconda/base/bin/python
+#!/usr/bin/python3
 
 # Import libraries
 import pandas as pd
 import sys
-import functools as ft
 
 
-#infilename = sys.argv[1]
-#outfilename = sys.argv[2]
-
-
+# Create a list of dfs
 dfs = []
 for infilename in sys.argv[1:]:
-    dfs.append(pd.read_csv(infilename, sep = ","))
+    dfs.append(pd.read_csv(infilename, sep = ",", index_col=False))
 
-ref = pd.read_csv("HXB2_refdata.csv", sep = ",")
-#ref = pd.DataFrame(ref["pos"], columns=["pos"])
 
-#dfs = [df.set_index('pos') for df in dfs] 
-#df = dfs[0].join(dfs[1:])
-#df = ft.reduce(lambda  left, right: left.join(right, how='outer', on='pos'), dfs)
-#df= ft.reduce(lambda left, right: pd.merge(left, right, on='pos'), dfs)
-#df= ft.reduce(lambda left, right: pd.merge(left, right, on = "pos", how = "outer"), dfs)
-#df = dfs[0]
-dfs.insert(0, ref)
-
+# Set a reference df as a df other columns should be joined
 df=dfs[0]
-print(len(dfs))
-print(dfs)
-
 for df_ in dfs[1:]:
-<<<<<<< HEAD
-    df = df.merge(df_, on="pos", how="right")
-=======
     df = df.merge(df_, on="pos", how="left")
->>>>>>> daeb4c2ca02a5132e8df582f97259da3108f67ea
+
+# Remove a sinity check column
+df.drop(["HXB2 base"], axis=1, inplace=True)
 
 
-df.to_csv("joined" + "_MAF" + ".csv", sep=",", index=False, encoding="utf-8")
+df = df.set_index("pos")
+# Transpose df
+df = df.T
+
+# Reset index
+#df = df.reset_index()
+print(df)
+
+# Make pos to be a header
+#headers = df.iloc[0]
+
+
+
+#df  = pd.DataFrame(df.values[1:], columns=headers)
+#print(df)
+
+# Create a clean csv file
+df.to_csv("joined" + "_MAF" + ".csv", sep=",", index = True, encoding="utf-8")
