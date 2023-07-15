6d4208f4a9348992dcd9bf09a98f770de8b32540
vera-rykalina <verarykalina@gmail.com>
2023-07-15 02:39:13 +0200

    Set and Reset indeses in join_mafs.py

--- Pipeline/Scripts/bin/join_mafs.py
+++ Pipeline/Scripts/bin/join_mafs.py
@@ -14,13 +14,25 @@ for infilename in sys.argv[1:]:
 # Set a reference df as a df other columns should be joined
 df=dfs[0]
 for df_ in dfs[1:]:
-    df = df.merge(df_, on="pos", how="left")
+    df = df.merge(df_, on="pos", how="right")
 
 # Remove a sinity check column
 df.drop(["HXB2 base"], axis=1, inplace=True)
 
+# Fill empty cells with zeros
+df.fillna(0, inplace = True,  downcast="infer")      
+
+# Set index to "pos" columns
+df = df.set_index("pos")
+
 # Transpose df
-df = df.T.reset_index()
+df = df.T
+print(df)
+
+# Reset index
+df = df.reset_index()
+print(df)
+
 
 # Create a clean csv file
-df.to_csv("joined" + "_MAF" + ".csv", sep=",", index = False, header = False, encoding="utf-8")
+df.to_csv("joined" + "_MAF" + ".csv", sep=",", header = True, index = False, encoding="utf-8")
