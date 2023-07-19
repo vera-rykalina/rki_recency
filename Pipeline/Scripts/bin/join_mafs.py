a58e3e6e4935b7307f5634163ae42d8c3361d744
vera-rykalina <verarykalina@gmail.com>
2023-07-19 17:37:51 +0200

    Stabilise merge() of tables

--- Pipeline/Scripts/bin/join_mafs.py
+++ Pipeline/Scripts/bin/join_mafs.py
@@ -5,21 +5,25 @@ import pandas as pd
 import sys
 
 
-# Create a list of dfs
+# Create a list of dfs except for HHXB2_refdata.csv
 dfs = []
 for infilename in sys.argv[1:]:
-    dfs.append(pd.read_csv(infilename, sep = ",", index_col=False))
+    name = infilename.rsplit("/")[-1] # gives a file name name.csv
+    list_of_substrings = name.split("_")
+    if "HXB2" in list_of_substrings:
+        df = pd.read_csv(infilename, sep = ",", index_col=False)
+    else:
+        dfs.append(pd.read_csv(infilename, sep = ",", index_col=False))
 
 
 # Set a reference df as a df other columns should be joined
-df=dfs[0]
-for df_ in dfs[1:]:
-    df = df.merge(df_, on="pos", how="right")
+for df_ in dfs[0:]:
+    df = df.merge(df_, on="pos", how="left")
 
 # Remove a sinity check column
 df.drop(["HXB2 base"], axis=1, inplace=True)
 
-# Fill empty cells with zeros
+# Fill empty cells with zeros (a need of this step should be double ckecked!!!)
 df.fillna(0, inplace = True,  downcast="infer")      
 
 # Set index to "pos" columns
@@ -27,12 +31,10 @@ df = df.set_index("pos")
 
 # Transpose df
 df = df.T
-print(df)
 
 # Reset index
 df = df.reset_index()
 print(df)
-
 
 # Create a clean csv file
 df.to_csv("joined" + "_MAF" + ".csv", sep=",", header = True, index = False, encoding="utf-8")
